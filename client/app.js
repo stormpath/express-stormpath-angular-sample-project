66f5ed20111bf92b547feb634fcbec34e3dc45e5
Robert <robert@stormpath.com>
2017-01-18 15:46:43 -0800

    Demonstrate dependency issue with upgraded UI-Router
    You should see the error:
    
    ```
    angular.js:4442 Uncaught Error: Unrecognised token store: localStorage
        at Object.getTokenStore (stormpath-sdk-angularjs.js:3394)
        at new StormpathOAuthToken (stormpath-sdk-angularjs.js:2322)
        at StormpathOAuthTokenProvider.$get (stormpath-sdk-angularjs.js:2476)
        at Object.invoke (angular.js:4478)
        at angular.js:4295
        at getService (angular.js:4437)
        at Object.invoke (angular.js:4469)
        at Object.enforcedReturnValue [as $get] (angular.js:4330)
        at Object.invoke (angular.js:4478)
        at angular.js:4295
    ```
    
    Moving the UI Router below stormpath will fix the prolem:
    
    ```
    angular.module('exampleApp', [
      'stormpath',
      'stormpath.templates’,
      'ui.router'
    ])
    ```

--- client/app.js
+++ client/app.js
@@ -11,12 +11,7 @@ angular.module('exampleApp', [
 
     $locationProvider.html5Mode(true);
 
-    /*
-     At the moment, JSON is not the default content type when posting forms, but
-     most of our framework integrations are expecting JSON, so we need to manually set
-     this.  JSON will be the default in the next major release of the Angular SDK.
-    */
-    STORMPATH_CONFIG.FORM_CONTENT_TYPE = 'application/json';
+    STORMPATH_CONFIG.ENDPOINT_PREFIX = 'https://lawful-heron.apps.stormpath.io';
   })
   .run(function($stormpath,$rootScope,$state){
 
